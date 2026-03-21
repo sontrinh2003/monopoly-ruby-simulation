9b61b1094139898ae7b1102b6ca19bd3bbc368f6
sontrinh2003 <hoangson200403@gmail.com>
2026-03-22 00:18:48 +1100

    Load json data file

--- board.rb
+++ board.rb
@@ -3,8 +3,14 @@
 # Methods: Initialize board from source data, get space by position
 
 class Board
-  
+  attr_accessor :spaces
   def initialize(data)
     # Create spaces from data
+    @spaces = data.map { |space_data| space_data["type"] == "property" ? Property.new(space_data) : space_data }
+  end
+
+  def get_space(position)
+    # Return the space at the given position
+    return @spaces[position]
   end
 end
--- monopoly.rb
+++ monopoly.rb
@@ -8,7 +8,7 @@ Features:
 
 RULES
 - Players start with $16 and position 0
-- Players take turns rolling a die (1-6) and moving forward
+- Players take turns rolling a dice (1-6) and moving forward
 - When passing GO (position 0), players receive $1, but not on their first move
 - The game ends when a player goes bankrupt (money < 0)
 - The winner is the player with the most money at the end of the game
@@ -23,16 +23,67 @@ Rent:
 
 =end
 
+require 'json'
+require_relative 'board'
+require_relative 'player'
+require_relative 'property'
+
 class MonopolyGame
 	attr_accessor :players, :board, :roll_data
 
-	def initialize(players, board)
+	def initialize(players, board, roll_data)
 		@players = players
 		@board = board
-		@roll_data = []
+		@roll_data = roll_data
 	end
 
 	def play
-		# Implement game logic: roll die, move player, handle landing on spaces
+		# Implement game logic: roll dice, move player, handle landing on spaces
+		# Loop through players and rolls until a player goes bankrupt, then determine the winner
+		@board.spaces.each do |space|
+			puts space
+		end
+		@roll_data.each do |roll|
+			puts roll
+		end
 	end
 end
+
+def main
+	# Initialize board and players, then start the game
+	
+	# Load board data from file, create Board object
+	print "Enter the file name of the Monopoly board: "
+  board_file = gets.chomp
+	while File.exist?(board_file) == false || File.extname(board_file) != ".json"
+		puts "File not exists or invalid."
+		print "Enter the file name of the Monopoly board: "
+		board_file = gets.chomp
+	end
+	board_data = JSON.parse(File.read(board_file))
+	board = Board.new(board_data)
+
+	# Load roll data from file
+	print "Enter the file name of dice rolls data: "
+	roll_file = gets.chomp
+	while File.exist?(roll_file) == false	|| File.extname(roll_file) != ".json"
+		puts "File not exists or invalid."
+		print "Enter the file name of dice rolls data: "
+		roll_file = gets.chomp
+	end
+	roll_data = JSON.parse(File.read(roll_file))
+
+	# Create players
+	players = [
+		Player.new("Peter"),
+		Player.new("Billy"),
+		Player.new("Charlotte"),
+		Player.new("Sweedal")
+	]
+
+	# Create game instance and start playing
+	game = MonopolyGame.new(players, board, roll_data)
+	game.play
+end
+
+main()
